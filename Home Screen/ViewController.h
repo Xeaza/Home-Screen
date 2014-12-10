e8f6ad8b05e3595e1ce10a83ed8c032728bf70d4
xeaza <taylorwrightsanson@gmail.com>
2014-12-10 11:47:57 -0500

    Initial Commit

---
+++ Home Screen/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 12/10/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Home ScreenTests/Home_ScreenTests.m
@@ -0,0 +1,40 @@
+//
+//  Home_ScreenTests.m
+//  Home ScreenTests
+//
+//  Created by Taylor Wright-Sanson on 12/10/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Home_ScreenTests : XCTestCase
+
+@end
+
+@implementation Home_ScreenTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
