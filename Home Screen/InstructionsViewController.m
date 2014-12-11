f945c328284bece322d3b9f0fdc22381dabd606e
xeaza <taylorwrightsanson@gmail.com>
2014-12-11 14:24:03 -0500

    Adding back button action to dismiss modal view. Hooking up InstructionsVC to View in Storyboard.

--- Home Screen/InstructionsViewController.m
+++ Home Screen/InstructionsViewController.m
@@ -10,12 +10,15 @@
 
 @implementation InstructionsViewController
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
 }
-*/
+
+- (IBAction)onBackButtonPressed:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 
 @end
