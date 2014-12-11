be2d833c610abb30b524255512a65260ce4d6f65
xeaza <taylorwrightsanson@gmail.com>
2014-12-11 14:21:08 -0500

    Adding Instructions ViewController

---
+++ Home Screen/InstructionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  InstructionsViewController.h
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 12/11/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstructionsViewController : UIViewController
+
+@end
---
+++ Home Screen/InstructionsViewController.m
@@ -0,0 +1,21 @@
+//
+//  InstructionsViewController.m
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 12/11/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "InstructionsViewController.h"
+
+@implementation InstructionsViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
