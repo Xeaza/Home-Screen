4af9cec6ffc3df2330af9b969b9473a5a83aad73
xeaza <taylorwrightsanson@gmail.com>
2014-12-16 17:38:40 -0500

    Adding tips and tricks view controller

---
+++ Home Screen/TipsAndTricksViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipsAndTricksViewController.h
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 12/16/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipsAndTricksViewController : UIViewController
+
+@end
---
+++ Home Screen/TipsAndTricksViewController.m
@@ -0,0 +1,27 @@
+//
+//  TipsAndTricksViewController.m
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 12/16/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "TipsAndTricksViewController.h"
+
+@interface TipsAndTricksViewController ()
+
+@end
+
+@implementation TipsAndTricksViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
