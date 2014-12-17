a45291c0320e93f985265f0355321342491251b0
xeaza <taylorwrightsanson@gmail.com>
2014-12-17 16:18:19 -0500

    Adding custom drawing code for chat bubble

--- Home Screen/ChatBubble.m
+++ Home Screen/ChatBubble.m
@@ -10,12 +10,37 @@
 
 @implementation ChatBubble
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)drawRect:(CGRect)rect
+{
+    UIColor* white = [UIColor colorWithRed: 1 green: 1 blue: 1 alpha: 1];
+
+    // Rectangle Drawing
+    UIBezierPath* rectanglePath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(10.14, 3.81, 293, 200) cornerRadius: 5];
+    [white setFill];
+    [rectanglePath fill];
+
+    // Corner Drawing
+    UIBezierPath* cornorArrowPath = UIBezierPath.bezierPath;
+    [cornorArrowPath
+     moveToPoint: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height - 7)];
+
+    [cornorArrowPath
+     addCurveToPoint: CGPointMake(rectanglePath.bounds.origin.y + 0.07, rectanglePath.bounds.size.height + 3) controlPoint1: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height - 7) controlPoint2: CGPointMake(7.88, rectanglePath.bounds.size.height)];
+
+    [cornorArrowPath
+     addCurveToPoint: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height)
+     controlPoint1: CGPointMake(-0.12, rectanglePath.bounds.size.height + 6)
+     controlPoint2: CGPointMake(10.88, rectanglePath.bounds.size.height - 7 + 6)];
+
+    [cornorArrowPath
+     addLineToPoint: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height - 7)];
+
+    [cornorArrowPath closePath];
+    [white setFill];
+    [cornorArrowPath fill];
+    [white setStroke];
+    cornorArrowPath.lineWidth = 1;
+    [cornorArrowPath stroke];
 }
-*/
 
 @end
