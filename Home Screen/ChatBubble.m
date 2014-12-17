223b20f44e6b784aae78f550265b5972a5449408
xeaza <taylorwrightsanson@gmail.com>
2014-12-17 17:31:09 -0500

    Updated size to chat bubble to be the size of the frame it is created as

--- Home Screen/ChatBubble.m
+++ Home Screen/ChatBubble.m
@@ -14,28 +14,19 @@
 {
     UIColor* white = [UIColor colorWithRed: 1 green: 1 blue: 1 alpha: 1];
 
-    // Rectangle Drawing
-    UIBezierPath* rectanglePath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(10.14, 3.81, 293, 200) cornerRadius: 5];
+    //// Rectangle Drawing
+    UIBezierPath* rectanglePath = [UIBezierPath bezierPathWithRoundedRect: CGRectMake(7, 10, self.frame.size.width - 20, self.frame.size.height - 20) cornerRadius: 5];
     [white setFill];
     [rectanglePath fill];
 
-    // Corner Drawing
+    //// Bezier Drawing
     UIBezierPath* cornorArrowPath = UIBezierPath.bezierPath;
-    [cornorArrowPath
-     moveToPoint: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height - 7)];
-
-    [cornorArrowPath
-     addCurveToPoint: CGPointMake(rectanglePath.bounds.origin.y + 0.07, rectanglePath.bounds.size.height + 3) controlPoint1: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height - 7) controlPoint2: CGPointMake(7.88, rectanglePath.bounds.size.height)];
-
-    [cornorArrowPath
-     addCurveToPoint: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height)
-     controlPoint1: CGPointMake(-0.12, rectanglePath.bounds.size.height + 6)
-     controlPoint2: CGPointMake(10.88, rectanglePath.bounds.size.height - 7 + 6)];
-
-    [cornorArrowPath
-     addLineToPoint: CGPointMake(rectanglePath.bounds.origin.x + 0.88, rectanglePath.bounds.size.height - 7)];
-
+    [cornorArrowPath moveToPoint: CGPointMake(7.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height - 10.84)];
+    [cornorArrowPath addCurveToPoint: CGPointMake(0.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height - .84) controlPoint1: CGPointMake(7.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height - 10.84) controlPoint2: CGPointMake(4.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height - 3.84)];
+    [cornorArrowPath addCurveToPoint: CGPointMake(7.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height - 3.84) controlPoint1: CGPointMake(-3.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height + 2.16) controlPoint2: CGPointMake(7.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height - 3.84)];
+    [cornorArrowPath addLineToPoint: CGPointMake(7.88, rectanglePath.bounds.origin.y + rectanglePath.bounds.size.height - 10.84)];
     [cornorArrowPath closePath];
+
     [white setFill];
     [cornorArrowPath fill];
     [white setStroke];
