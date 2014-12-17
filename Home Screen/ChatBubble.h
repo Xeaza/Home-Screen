b30535ae782e3b07d95090526436c2121faaf793
xeaza <taylorwrightsanson@gmail.com>
2014-12-17 16:17:42 -0500

    Adding ChatBubble class to create views that look like chat bubbles

---
+++ Home Screen/ChatBubble.h
@@ -0,0 +1,13 @@
+//
+//  ChatBubble.h
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 12/17/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChatBubble : UIView
+
+@end
---
+++ Home Screen/ChatBubble.m
@@ -0,0 +1,21 @@
+//
+//  ChatBubble.m
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 12/17/14.
+//  Copyright (c) 2014 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "ChatBubble.h"
+
+@implementation ChatBubble
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
