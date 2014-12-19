f92da428e81a54356a4e578db5b2178e2f558861
xeaza <taylorwrightsanson@gmail.com>
2014-12-19 13:32:17 -0500

    Adding dictionary to hold tips and tricks

--- Home Screen/TipsAndTricksViewController.m
+++ Home Screen/TipsAndTricksViewController.m
@@ -12,6 +12,10 @@
 @interface TipsAndTricksViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *gifImageView;
+@property (weak, nonatomic) IBOutlet UILabel *instructionLabel;
+@property (strong, nonatomic) NSArray *tipsArray;
+@property (strong, nonatomic) NSArray *urlArray;
+@property (strong, nonatomic) NSMutableDictionary *tipsDictionary;
 
 @end
 
@@ -19,10 +23,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"ee" withExtension:@"gif"];
+
+    self.tipsDictionary = [NSMutableDictionary dictionaryWithObjectsAndKeys:@[url, @"Type two 'e's to load all available emoji beging with your most recently used."], @"ee", nil];
 
     // Example code for animating gif
-    NSURL *url = [[NSBundle mainBundle] URLForResource:@"ee" withExtension:@"gif"];
-    self.gifImageView.image = [UIImage animatedImageWithAnimatedGIFURL:url];
+    self.gifImageView.image = [UIImage animatedImageWithAnimatedGIFURL:self.tipsDictionary[@"ee"][0]];
+
+    self.instructionLabel.text = self.tipsDictionary[@"ee"][1];
 }
 
 - (void)didReceiveMemoryWarning {
