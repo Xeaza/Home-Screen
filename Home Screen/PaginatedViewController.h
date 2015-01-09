a899346acc80bc622e51b07bb190dd46bd389ef4
xeaza <taylorwrightsanson@gmail.com>
2015-01-09 14:22:14 -0500

    Adding a pageViewController class to manage view controllers

---
+++ Home Screen/PaginatedViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaginatedViewController.h
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 1/8/15.
+//  Copyright (c) 2015 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaginatedViewController : UIViewController
+
+@end
---
+++ Home Screen/PaginatedViewController.m
@@ -0,0 +1,37 @@
+//
+//  PaginatedViewController.m
+//  Home Screen
+//
+//  Created by Taylor Wright-Sanson on 1/8/15.
+//  Copyright (c) 2015 Taylor Wright-Sanson. All rights reserved.
+//
+
+#import "PaginatedViewController.h"
+
+@interface PaginatedViewController ()
+
+@end
+
+@implementation PaginatedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
